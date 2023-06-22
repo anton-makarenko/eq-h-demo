db463f57252b0bb4b4fe1339c1474d0a23b54881
Anton Makarenko <makarenkoanton2013@gmail.com>
2023-06-23 01:42:30 +0300

    Added simple controller

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("java")
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.springframework.boot") version "3.1.0"
 }
+
+val springBootVersion = "3.1.0"
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
@@ -10,6 +14,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-parent:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
